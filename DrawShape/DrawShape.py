e7d1845b7f9cf5fea954e5c36cd48af8391c4aeb
Malachi S <malachis1447@gmail.com>
2023-08-10 08:35:01 +0000

    Add first scripts for learning Python

---
+++ DrawShape/DrawShape.py
@@ -0,0 +1,9 @@
+# DrawShape.py
+# Kain M. Snyder
+# Thu Aug 10 2023 0223 MDT
+
+# Print a shape, just to get familiar with the print() statement
+
+print("  /\\"),
+print(" /  \\"),
+print("/____\\"),
---
+++ hello-world/helloWorld.py
@@ -0,0 +1,44 @@
+#Python3 Hello-World.py
+# Kain M Snyder
+# Thu, Aug 10th 2023
+# 0009 MDT
+
+# Define variables
+
+x = 5
+y = "Hello World",
+GraveShift = "Malachi", "Noel", "Matt", "Jerry"
+a, b, c, d = GraveShift
+
+# 1: Print Hello World
+
+def HelloWorld():
+    print ("Hello World"),
+HelloWorld()
+
+# Conditional Print (if a statement is true, print text)
+
+def FiveTwo():
+    if 5 > 2:
+        print("Five is greater than two!"),
+FiveTwo()
+
+# Check the value of X and print a statement dependent on the value
+
+def XvsTwo():
+    if x > 2:
+        print("X is greater than 2"),
+    elif x < 2:
+        print("X is less than 2"),
+    else:
+        print("X is equal to 2"),
+XvsTwo()
+
+# set a series of values in variables and print them
+
+def PrintGraves():
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+PrintGraves()
